0ae9b266e22ea3777cb67e90917da72bb609cfcb
Ygor <ygor_sy@hotmail.com>
2019-11-24 23:29:57 -0300

    Atualizando a postagem

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter/services.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'BReddit',
       home: Scaffold(
         appBar: Cabecalho(),
-        body: buildBody(),
+        body: MyScreen(),
         bottomNavigationBar: Rodape(),
         backgroundColor: Color.fromRGBO(255, 255, 255, 0.9),
       ),
@@ -26,16 +27,42 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Widget buildBody() {
-  return new Container(
+class MyScreen extends StatefulWidget {
+  @override
+   createState() => _MyScreen();
+}
+
+class _MyScreen extends State {
+
+  //Criar algo para ler json e achar um jeito de comunicar com o postagens para integrar
+  //crair uma interface que aceita postagens do tipo texto ou img
+  @override
+  Widget build(BuildContext context) {
+    return Container(
     margin: EdgeInsets.symmetric(vertical: 0, horizontal: 3),
     child: new ListView.builder(
-        itemCount: 20,
+        itemCount: 2,
         scrollDirection: Axis.vertical,
         itemBuilder: (BuildContext ctxt, int index) {
           return Container(
-            child:Postagens(),
+            child: Postagens(),
           );
         }),
   );
+  }
 }
+
+// Widget buildBody() {
+
+//   return new Container(
+//     margin: EdgeInsets.symmetric(vertical: 0, horizontal: 3),
+//     child: new ListView.builder(
+//         itemCount: 4,
+//         scrollDirection: Axis.vertical,
+//         itemBuilder: (BuildContext ctxt, int index) {
+//           return Container(
+//             child: Postagens(),
+//           );
+//         }),
+//   );
+// }
--- lib/postagens.dart
+++ lib/postagens.dart
@@ -2,7 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-class Postagens extends StatelessWidget {
+class Postagens extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _Postagens();
+  }
+}
+
+class _Postagens extends State<Postagens> {
+  bool botaoGostei = false;
+  bool botaoNGostei = false;
+  int likeCounter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,7 +27,8 @@ class Postagens extends StatelessWidget {
                 Container(
                   width: MediaQuery.of(context).size.width * 0.12,
                   child: Icon(Icons.account_circle,
-                      size: 40, color: Color.fromRGBO(112, 112, 112, 1)),
+                      size: MediaQuery.of(context).size.width * 0.10, 
+                      color: Color.fromRGBO(112, 112, 112, 1)),
                 ),
                 Container(
                   width: MediaQuery.of(context).size.width * 0.75,
@@ -30,7 +42,8 @@ class Postagens extends StatelessWidget {
                           style: TextStyle(
                               fontFamily: 'Roboto',
                               color: Color.fromRGBO(112, 112, 112, 1),
-                              fontSize: 15),
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.035),
                         ),
                       ),
                       Container(
@@ -41,7 +54,8 @@ class Postagens extends StatelessWidget {
                             style: TextStyle(
                                 fontFamily: 'Roboto',
                                 color: Color.fromRGBO(112, 112, 112, 1),
-                                fontSize: 10),
+                                fontSize:
+                                    MediaQuery.of(context).size.width * 0.028),
                           )),
                     ],
                   ),
@@ -49,27 +63,37 @@ class Postagens extends StatelessWidget {
                 Container(
                   width: MediaQuery.of(context).size.width * 0.10,
                   child: IconButton(
-                    icon: Icon(Icons.more_horiz),
-                    onPressed: () => AlertDialog(content: Text("aaaaa")),
+                    icon: Icon(Icons.more_horiz,
+                      size: MediaQuery.of(context).size.width * 0.08,  
+                      color: Color.fromRGBO(112, 112, 112, 1)),
+                    onPressed: () => {},
                   ),
                 ),
               ],
             ),
             Container(
               padding: EdgeInsets.symmetric(
-                  vertical: MediaQuery.of(context).size.width * 0.1),
-              width: MediaQuery.of(context).size.width * 0.95,
-              height: MediaQuery.of(context).size.width * 0.25,
+                  vertical: MediaQuery.of(context).size.width * 0.01),
+              width: MediaQuery.of(context).size.width * 0.99,
+              margin: EdgeInsets.symmetric(
+                  vertical: MediaQuery.of(context).size.width * 0.05),
               child: Text(
-                "Um Corgi muito fofinho",
+                "Um Corgi",
+                softWrap: true,
                 textAlign: TextAlign.left,
+                style: TextStyle(
+                    fontFamily: 'Roboto',
+                    color: Color.fromRGBO(112, 112, 112, 1),
+                    fontSize: MediaQuery.of(context).size.width * 0.05),
               ),
             ),
             Container(
+              color: Colors.black,
               child: Image.network(
-                "https://i.imgur.com/jdTOY2j.jpg",
-                width: MediaQuery.of(context).size.width * 0.99,
-                height: MediaQuery.of(context).size.width * 0.95,
+                "https://media.giphy.com/media/7Y66VN3rtkPtu/giphy.gif",
+                //width: MediaQuery.of(context).size.width * 0.99,
+                //height: MediaQuery.of(context).size.width * 0.95,
+                scale: 1.0,
               ),
             ),
             Row(
@@ -79,20 +103,36 @@ class Postagens extends StatelessWidget {
                     icon: Icon(
                       Icons.sentiment_satisfied,
                       size: 30,
+                      color: this.botaoGostei
+                          ? Colors.blue
+                          : Color.fromRGBO(112, 112, 112, 1),
                     ),
                     alignment: Alignment.centerRight,
                     color: Color.fromRGBO(112, 112, 112, 1),
+                    onPressed: () {
+                      //Precisa arrumar
+                      setState(() {
+                        if (!this.botaoGostei) {
+                          this.botaoGostei = true;
+                          this.botaoNGostei = false;
+                          likeCounter++;
+                        } else {
+                          this.botaoGostei = false;
+                          likeCounter--;
+                        }
+                      });
+                    },
                   ),
                   width: MediaQuery.of(context).size.width * 0.12,
                 ),
                 Container(
                   child: Text(
-                    "1000",
+                    likeCounter.toString(),
                     textAlign: TextAlign.center,
                     style: TextStyle(
                         fontFamily: 'Roboto',
                         color: Color.fromRGBO(112, 112, 112, 1),
-                        fontSize: 15),
+                        fontSize: MediaQuery.of(context).size.width * 0.035),
                   ),
                   width: MediaQuery.of(context).size.width * 0.14,
                 ),
@@ -101,8 +141,24 @@ class Postagens extends StatelessWidget {
                       icon: Icon(
                         Icons.sentiment_dissatisfied,
                         size: 30,
+                        color: this.botaoNGostei
+                            ? Colors.red
+                            : Color.fromRGBO(112, 112, 112, 1),
                       ),
                       alignment: Alignment.centerRight,
+                      onPressed: () {
+                        //Precisa arrumar
+                        setState(() {
+                          if (!this.botaoNGostei) {
+                            this.botaoNGostei = true;
+                            this.botaoGostei = false;
+                            likeCounter--;
+                          } else {
+                            likeCounter++;
+                            this.botaoNGostei = false;
+                          }
+                        });
+                      },
                       color: Color.fromRGBO(112, 112, 112, 1)),
                   width: MediaQuery.of(context).size.width * 0.12,
                 ),
@@ -113,8 +169,20 @@ class Postagens extends StatelessWidget {
                         size: 30,
                       ),
                       alignment: Alignment.centerRight,
+                      onPressed: () {},
                       color: Color.fromRGBO(112, 112, 112, 1)),
-                  width: MediaQuery.of(context).size.width * 0.60,
+                  width: MediaQuery.of(context).size.width * 0.49,
+                ),
+                Container(
+                  child: Text(
+                    "1000",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Roboto',
+                        color: Color.fromRGBO(112, 112, 112, 1),
+                        fontSize: MediaQuery.of(context).size.width * 0.035),
+                  ),
+                  width: MediaQuery.of(context).size.width * 0.10,
                 ),
               ],
             ),
